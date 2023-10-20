35c4c1d152abb71782502bebaed8846b23a37953
YJbrd <yjobard@rt-iut.re>
2023-10-20 17:18:25 +0400

    page de creation du profile

--- Joton/src/main/java/View/CreateProfile.java
+++ Joton/src/main/java/View/CreateProfile.java
@@ -1,6 +1,35 @@
 package View;
-import package Data; 
 
-public class CreateProfile {
+//import Data.user;
+import javax.swing.*;  
+public class CreateProfile {  
+public static void main(String[] args) {  
+JFrame f=new JFrame();//creating instance of JFrame 
 
-}
+JTextField Nusername= new JTextField("Enter name :");
+JTextField Nusersurname= new JTextField("Enter surname :");
+JTextField Nuseremail= new JTextField("Enter email :");
+JTextField Nuserpassword= new JTextField("Enter password :");
+
+JButton buttonvalid=new JButton("Create profile");//creating instance of JButton
+
+Nusername.setBounds(10,20,150,40);
+Nusersurname.setBounds(10,70,150,40);
+Nuseremail.setBounds(10,120,150,40);
+Nuserpassword.setBounds(10,170,150,40);
+buttonvalid.setBounds(300,230,180, 50);//x axis, y axis, width, height  
+
+f.add(buttonvalid);//adding button in JFrame 
+f.add(Nusername);
+f.add(Nusersurname);
+f.add(Nuseremail);
+f.add(Nuserpassword);
+
+          
+f.setSize(500,400); 
+f.setLayout(null);//using no layout managers  
+f.setVisible(true);//making the frame visible  
+}  
+
+// créer une méthode créant un nouveau user
+}  
