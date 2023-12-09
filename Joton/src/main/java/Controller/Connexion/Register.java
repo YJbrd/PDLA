9322b3f9c1053e80e2f60cd18e9f85dcc8f231fb
YJbrd <y.jboard@rt-iut.re>
2023-12-09 16:43:59 +0100

    enregistrement via application

--- Joton/src/main/java/Controller/Connexion/Register.java
+++ Joton/src/main/java/Controller/Connexion/Register.java
@@ -8,44 +8,45 @@ import java.util.Scanner;
 public class Register {
 
 	// Méthode pour enregistrer un nouvel utilisateur
-    public static void enregistrerUtilisateur(String nomUtilisateur, String motDePasse) {
-        String query = "INSERT INTO User (UserPseudo, UserPassword) VALUES (?, ?)";
-        try (Connection connection = ConnexionDataBase.getConnexionDataBase();
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+	public static void enregistrerUtilisateur(String nomUtilisateur, String motDePasse, String typeUtilisateur) {
+	    String query = "INSERT INTO User (UserPseudo, UserPassword, UserType) VALUES (?, ?, ?)";
+	    try (Connection connection = ConnexionDataBase.getConnexionDataBase();
+	         PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
-            // Paramétrer les valeurs
-            preparedStatement.setString(1, nomUtilisateur);
-            preparedStatement.setString(2, motDePasse);
+	        // Paramétrer les valeurs
+	        preparedStatement.setString(1, nomUtilisateur);
+	        preparedStatement.setString(2, motDePasse);
+	        preparedStatement.setString(3, typeUtilisateur);
 
-            // Exécuter la requête
-            int rowCount = preparedStatement.executeUpdate();
+	        // Exécuter la requête
+	        int rowCount = preparedStatement.executeUpdate();
 
-            // Vérifier si l'enregistrement a réussi
-            if (rowCount > 0) {
-                System.out.println("Enregistrement réussi !");
-            } else {
-                System.out.println("Échec de l'enregistrement. Veuillez réessayer.");
-            }
+	        // Vérifier si l'enregistrement a réussi
+	        if (rowCount > 0) {
+	            System.out.println("Enregistrement réussi !");
+	        } else {
+	            System.out.println("Échec de l'enregistrement. Veuillez réessayer.");
+	        }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Demander à l'utilisateur de saisir un nom d'utilisateur et un mot de passe
-        System.out.print("Nom d'utilisateur : ");
-        String nomUtilisateur = scanner.nextLine();
-
-        System.out.print("Mot de passe : ");
-        String motDePasse = scanner.nextLine();
-
-        // Appeler la fonction pour enregistrer l'utilisateur
-        enregistrerUtilisateur(nomUtilisateur, motDePasse);
-
-        // Fermer le scanner
-        scanner.close();
-    }
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        // Demander à l'utilisateur de saisir un nom d'utilisateur et un mot de passe
+//        System.out.print("Nom d'utilisateur : ");
+//        String nomUtilisateur = scanner.nextLine();
+//
+//        System.out.print("Mot de passe : ");
+//        String motDePasse = scanner.nextLine();
+//
+//        // Appeler la fonction pour enregistrer l'utilisateur
+//        enregistrerUtilisateur(nomUtilisateur, motDePasse);
+//
+//        // Fermer le scanner
+//        scanner.close();
+//    }
 }
---
+++ Joton/src/main/java/View/FrameRegister.java
@@ -0,0 +1,62 @@
+package View;
+
+import Controller.Connexion.Register;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FrameRegister extends JFrame {
+
+    private JTextField pseudoField;
+    private JPasswordField motDePasseField;
+    private JComboBox<String> typeUtilisateurComboBox;
+
+    public FrameRegister() {
+        setTitle("Page d'Enregistrement");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        pseudoField = new JTextField(20);
+        motDePasseField = new JPasswordField(20);
+
+        typeUtilisateurComboBox = new JComboBox<>(new String[]{"Bénévole", "Validateur", "Vulnérable"});
+
+        JButton boutonEnregistrement = new JButton("S'enregistrer");
+
+        boutonEnregistrement.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pseudo = pseudoField.getText();
+                String motDePasse = new String(motDePasseField.getPassword());
+                String typeUtilisateur = (String) typeUtilisateurComboBox.getSelectedItem();
+
+                Controller.Connexion.Register.enregistrerUtilisateur(pseudo, motDePasse, typeUtilisateur);
+
+                JOptionPane.showMessageDialog(FrameRegister.this,
+                        "Enregistrement réussi!\nPseudo: " + pseudo + "\nMot de Passe: " + motDePasse + "\nType d'Utilisateur: " + typeUtilisateur,
+                        "Informations d'Enregistrement",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        panel.add(new JLabel("Pseudo:"));
+        panel.add(pseudoField);
+        panel.add(new JLabel("Mot de Passe:"));
+        panel.add(motDePasseField);
+        panel.add(new JLabel("Type d'Utilisateur:"));
+        panel.add(typeUtilisateurComboBox);
+        panel.add(boutonEnregistrement);
+
+        add(panel);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new FrameRegister());
+    }
+}
