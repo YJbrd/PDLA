c1acbceb75126fc09a6c0d090d5e2d2511342464
Elisabeth <elisabeth.maton@etu.iut-tlse3.fr>
2023-12-09 16:44:53 +0100

    coazjhdsd

--- Joton/src/main/java/View/Welcome.java
+++ Joton/src/main/java/View/Welcome.java
@@ -1,6 +1,8 @@
 package View;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -13,6 +15,11 @@ import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 public class Welcome extends JFrame {
 
     private static final long serialVersionUID = -4939544011287453046L;
+    
+    JButton login = new JButton( "Me connecter" );
+    JButton register = new JButton( "Créer un nouveau profil" );
+
+
 
     public Welcome() {
         super( "Bienvenue sur Joton !" );
@@ -21,13 +28,12 @@ public class Welcome extends JFrame {
         this.setLocationRelativeTo( null );
 
         JPanel contentPane = (JPanel) this.getContentPane();
-        contentPane.setLayout( new FlowLayout() );
-        
-        JButton login = new JButton( "Me connecter" );
+        contentPane.setLayout(new FlowLayout());
         contentPane.add( login );
-        
-        JButton register = new JButton( "Créer un nouveau profil" );
         contentPane.add( register );
+        
+        //ActionListener controleur = new ActionListener(this); //controleur qui écoute tous les clics sur l'interface graphique
+
         
         //JCheckBox kindOfUser = new JCheckBox( "Type de profil" );
         //contentPane.add( kindOfUser );
@@ -35,6 +41,15 @@ public class Welcome extends JFrame {
         //JTextField txtEditMe = new JTextField( "Edit me!" );
         //txtEditMe.setPreferredSize( new Dimension( 120,  30 ) );
         //contentPane.add( txtEditMe );
+        
+        login.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				// TODO Auto-generated method stub
+				
+			}			   
+		
+        });
     }
     
     public static void main(String[] args) throws Exception {
@@ -44,6 +59,6 @@ public class Welcome extends JFrame {
         // Start my window
         Welcome myWindow = new Welcome();
         myWindow.setVisible( true );
-    }
+    } 
 
 }
